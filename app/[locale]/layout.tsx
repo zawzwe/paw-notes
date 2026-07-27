75f292e4886cc1e191453d8c0076b0289bc05496
Arakai <1099417497@qq.com>
2026-07-27 18:33:46 +0800

    feat: 嵌入 TikTok Pixel (D9JCFSRC77U123LAKATG) 用于广告投流

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 import "../globals.css";
 
 export function generateStaticParams() {
@@ -47,6 +48,9 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};ttq.load('D9JCFSRC77U123LAKATG');ttq.page();}(window,document,'ttq');`}
+        </Script>
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
             attribute="class"
